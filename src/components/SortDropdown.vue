5e1a9593fbf81f80ad5a390ddf755fef4acb9041
evvvritt <evv.info@gmail.com>
2022-12-09 19:20:24 +0100

    toucup mobile sort layout

--- src/components/SortDropdown.vue
+++ src/components/SortDropdown.vue
@@ -5,35 +5,18 @@
       .pl-4.pr-3 🗼
     
     button.network-switcher__btn.h-8.rounded-full.bg-accent1.flex.items-center.border.border-accent2.block.pl-6.rounded-full.flex.items-center.pb-1(@click.prevent="menuVisible = !menuVisible")
-      //- addr(:address="$store.state.address")
       .whitespace-nowrap.pointer-events-none {{ $route.query.sort }}
       svg-chevron-down.w-6.h-6.mx-3.mt-1.pointer-events-none(strokeWidth="1.25")
 
   .relative(v-if="menuVisible", v-click-outside="onClickOutsideMenu")
     .absolute.bottom-0.sm_bottom-auto.sm_top-0.right-1.py-2
       ul.bg-accent1.text-accent2.border.rounded-lg.py-3.pb-4.whitespace-nowrap.sm_shadow
-        //- .text-xxs.pl-3.pb-1b networks:
         li
           button.block.px-5.rounded-full(@click.prevent="setSort('updated')") updated
         li
           button.block.px-5.rounded-full(@click.prevent="setSort('oldest')") oldest
         li  
           button.block.px-5.rounded-full(@click.prevent="setSort('newest')") newest
-        //- li
-        //-   button.block.pl-6.pr-6.rounded-full(@click.prevent="setNetwork('ethereum')") ethereum
-        //- li
-        //-   button.block.pl-6.pr-6.rounded-full(@click.prevent="setNetwork('optimism')") optimism
-        //- li(v-show="!testnetsVisible")
-        //-   button.block.pl-6.pr-6.rounded-full(@click.prevent="testnetsVisible = true")
-        //-     span.opacity-40.mouse_hover_opacity-100.text-xs testnets
-        //- li(v-show="testnetsVisible")
-        //-   button.block.pl-6.pr-6.rounded-full(@click.prevent="setNetwork('goerli')")
-        //-     span.text-xxs.mr-2.opacity-40 testnet
-        //-     | goerli
-        //- li(v-show="testnetsVisible")
-        //-   button.block.pl-6.pr-6.rounded-full(@click.prevent="setNetwork('optimism-goerli')")
-        //-     span.text-xxs.mr-2.opacity-40 testnet
-        //-     | optimism-goerli
 
 </template>
 
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 article.index
-  header.mt-72.lg_mt-60.px-6.lg_pl-20.lg_pr-12.relative.z-10
+  header.mt-64.lg_mt-60.px-6.lg_pl-20.lg_pr-12.relative.z-10
 
     nav.flex.flex-wrap.text-md.justify-center.sm_justify-start.items-center
 
@@ -14,10 +14,8 @@ article.index
 
       network-switcher(:initNetworkName="$route.params.networkName", @change="onNetworkChange")
       
-      //- .flex-1.sm_flex-none.sm_w-16.border-t
-
       template(v-if="$route.name === 'network-index'")
-        .fixed.z-40.bottom-20.left-0.w-full.sm_static.sm_w-auto.flex-1.flex.justify-center.sm_justify-end.mt-4.sm_mt-0
+        .fixed.z-40.bottom-12.left-0.w-full.sm_static.sm_w-auto.flex-1.flex.justify-center.sm_justify-end.mt-4.sm_mt-0
           sort-dropdown-vue
 
   section.mt-9
